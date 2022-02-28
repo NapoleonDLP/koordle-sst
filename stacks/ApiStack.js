5b9b6abdf0ecae6295a445e4beb31465258c511c
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-28 13:32:17 -0800

    Create Api stack

---
+++ stacks/ApiStack.js
@@ -0,0 +1,29 @@
+import * as sst from '@serverless-stack/resources';
+
+export default class ApiStack extends sst.Stack {
+  api;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const { table } = props;
+
+    this.api = new sst.Api(this, 'Api', {
+      defaultFunctionProps: {
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+      routes: {
+        'POST /games': 'src/create.main'
+      },
+    });
+    // Allow the APi to access the table
+    this.api.attachPermissions([table]);
+
+    // Show the API endpoint int he output
+    this.addOutputs({
+      ApiEndpoint: this.api.url,
+    });
+  }
+}
