ccb18dc423ee843ffd247945922114cbe7ba7bb0
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-28 11:53:02 -0800

    Create Games dynamoDB table

---
+++ stacks/StorageStack.js
@@ -0,0 +1,19 @@
+import * from sst from '@serverless-stack/resources';
+
+export default class StorageStack extends sst.Stack {
+  // Public reference to the table
+  table;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Create the DynamoDB table
+    this.table = new sst.Table(this, 'Games', {
+      fields: {
+        userId: sst.TableFieldType.STRING,
+        gameId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: { patitionKey: 'userId', sortKey: 'gameId'},
+    });
+  }
+}
